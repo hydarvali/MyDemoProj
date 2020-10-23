414cc6109b96186ad046044fec4280dec85c5870
hydar <valisyed34@gmail.com>
2020-10-23 12:09:44 +0530

    Initial commit

---
+++ src/main/java/com/mydemoproj/MyDemoProj/MyDemoProjApplication.java
@@ -0,0 +1,13 @@
+package com.mydemoproj.MyDemoProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDemoProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDemoProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mydemoproj/MyDemoProj/MyDemoProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydemoproj.MyDemoProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
